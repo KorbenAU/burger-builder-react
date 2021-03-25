43b59d45ae22c940083875d2ae969d88702f0206
Korben Gao <yg@psychpress.com.au>
2021-03-26 09:22:45 +1100

    Finished burger ingredients component

--- src/containers/BurgerBuilder/BurgerBuilder.jsx
+++ src/containers/BurgerBuilder/BurgerBuilder.jsx
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
+import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+  state = {
+    ingredients: {
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0,
+    },
+    totalPrice: 4,
+    purchasable: false,
+    purchasing: false,
+  };
   render() {
     return (
       <>
-        <div>burger</div>
+        <Burger ingredients={this.state.ingredients} />
         <div>congroller</div>
       </>
     );
