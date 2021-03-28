121071ce1096c66a6cfd0a88b213c2fb8563fbd0
Korben <gaoyuan.hello@gmail.com>
2021-03-28 17:57:51 +1100

    Finished app routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Layout from './hoc/Layout/Layout';
 import Checkout from './containers/Checkout/Checkout';
 import { Route, Switch } from 'react-router-dom';
+import Orders from './containers/Orders/Orders';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Switch>
           <Route path='/' exact component={BurgerBuilder} />
           <Route path='/checkout' component={Checkout} />
+          <Route path='/orders' component={Orders} />
         </Switch>
       </Layout>
     </div>
