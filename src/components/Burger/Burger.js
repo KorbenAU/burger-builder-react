02f6318be0b7947729892b8669668de9c2133ff4
Korben <gaoyuan.hello@gmail.com>
2021-03-28 17:38:23 +1100

    Finished checkout page

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import classes from './Burger.module.css';
 import BurgerIngredient from './Ingredient/Ingredient';
+import { withRouter } from 'react-router-dom';
 
 const Burger = (props) => {
   let transformedIngredients = Object.keys(props.ingredients)
@@ -25,4 +26,4 @@ const Burger = (props) => {
   );
 };
 
-export default Burger;
+export default withRouter(Burger);
